32761fc22bdb664911849a69ae72847a39b8aa91
davion <daiwenyong@cngongbao.com>
2022-09-23 14:34:55 +0800

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 function Login(){
+    const [name,setName] = useState('')
+    const [pwd,setPwd] = useState('')
     return <div>
         Login
+        账号 <input value={name} onChange={(e)=>{setName(e.target.value)}}></input>
+        密码 <input value={pwd} onChange={(e)=>{setPwd(e.target.value)}}></input>
     </div>
 }
 export default {
